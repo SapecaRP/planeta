0bf292407429e1b6e706a8ff1fa012b25d5f0005
SapecaRP <163039608+SapecaRP@users.noreply.github.com>
2025-07-22 11:00:26 -0300

    Updated useManutencoes.ts

--- src/hooks/useManutencoes.ts
+++ src/hooks/useManutencoes.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../supabase/supabaseClient';
+import { supabase } from "./supabaseClient";
 import { Manutencao, ManutencaoFormData } from '../types';
 
 export function useManutencoes() {
